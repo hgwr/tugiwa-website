eacf9fb1d6aafc3697c923c32441b6b29b21cbe8
Shigeru Hagiwara <hgwrsgr@gmail.com>
2017-09-19 08:24:09 +0900

    adjust scrollspy

--- js/tugiwa.js
+++ js/tugiwa.js
@@ -1,9 +1,10 @@
 jQuery(document).ready(function($) {
 
-  var offset = 70;
+  var offset = 40;
 
   $('.navbar li a').click(function(event) {
     event.preventDefault();
+    $(this).blur();
     $($(this).attr('href'))[0].scrollIntoView();
     scrollBy(0, -offset);
   });
